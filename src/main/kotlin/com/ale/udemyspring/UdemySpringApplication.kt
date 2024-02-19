9e499842bb704c0620db0dd75da1ce1fdf584316
Alejandro1988 <ale.aaaf@gmail.com>
2024-02-19 09:23:44 -0300

    Commit inicial

---
+++ src/main/kotlin/com/ale/udemyspring/UdemySpringApplication.kt
@@ -0,0 +1,11 @@
+package com.ale.udemyspring
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class UdemySpringApplication
+
+fun main(args: Array<String>) {
+    runApplication<UdemySpringApplication>(*args)
+}
---
+++ src/test/kotlin/com/ale/udemyspring/UdemySpringApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.ale.udemyspring
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class UdemySpringApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
